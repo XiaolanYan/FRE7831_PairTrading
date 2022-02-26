e27123d7f8aab3954c0a4b50835be2d1aec605e5
Shiya Yang <tauriel.zjiale1988@gmail.com>
2022-02-26 13:12:27 -0500

    Update menu C, D

--- FRE7831_MarketData_JSON/main.cpp
+++ FRE7831_MarketData_JSON/main.cpp
@@ -87,7 +87,73 @@ int PopulateStockPairs(sqlite3* db, string pairtxt, vector<string>& PairOneSymbo
 	return 0;
 }
 
+int CreatePairPricesTable(sqlite3* db)
+{
+	string sql_CreateTable = string("CREATE TABLE IF NOT EXISTS PairPrices ")
+		+ "(symbol1 CHAR(20) NOT NULL, "
+		+ "symbol2 CHAR(20) NOT NULL, "
+		+ "date CHAR(20) NOT NULL, "
+		+ "open1 REAL NOT NULL, "
+		+ "close1 REAL NOT NULL, "
+		+ "open2 REAL NOT NULL, "
+		+ "close2 REAL NOT NULL, "
+		+ "profit_loss REAL NOT NULL, "
+		+ "PRIMARY KEY(symbol1, symbol2, date), "
+		+ "FOREIGN KEY(symbol1, date) REFERENCES PairOnePrices(symbol, date) "
+		+ "ON DELETE CASCADE ON UPDATE CASCADE,\n"
+		+ "FOREIGN KEY(symbol2, date) REFERENCES PairTwoPrices(symbol, date) "
+		+ "ON DELETE CASCADE ON UPDATE CASCADE,\n"
+		+ "FOREIGN KEY(symbol1, symbol2) REFERENCES StockPairs(symbol1, symbol2) "
+		+ "ON DELETE CASCADE ON UPDATE CASCADE"
+		+ ");";
+	if (ExecuteSQL(db, sql_CreateTable.c_str()) == -1) 
+		return -1;
+	cout << "Successfully create PairPrices Table." << endl;
 
+	return 0;
+}
+
+int PopulatePairPrices(sqlite3* db)
+{
+	string sql_insert = string("INSERT INTO PairPrices ")
+		+ "SELECT StockPairs.symbol1 as symbol1, StockPairs.symbol2 as symbol2, "
+		+ "PairOnePrices.date as date, "
+		+ "PairOnePrices.open as open1, PairOnePrices.close as close1, "
+		+ "PairTwoPrices.open as open2, PairTwoPrices.close as close2, "
+		+ "0 as profit_loss "
+		+ "FROM StockPairs, PairOnePrices, PairTwoPrices "
+		+ "WHERE (((StockPairs.symbol1 = PairOnePrices.symbol) AND "
+		+ "(StockPairs.symbol2 = PairTwoPrices.symbol)) AND "
+		+ "(PairOnePrices.date = PairTwoPrices.date)) "
+		+ "ORDER BY symbol1, symbol2;";
+	if (ExecuteSQL(db, sql_insert.c_str()) == -1) 
+		return -1;
+	cout << "Successfully populate PairPrices Table." << endl;
+
+	return 0;
+}
+
+int CalculateVolatility(sqlite3* db, string back_test_start_date)
+{
+	string calculate_volatility_for_pair = string("Update StockPairs SET volatility =")
+		+ "(SELECT(AVG((adjusted_close1/adjusted_close2)*(adjusted_close1/ adjusted_close2)) - AVG(adjusted_close1/adjusted_close2)*AVG(adjusted_close1/adjusted_close2)) as variance "
+		+ "FROM (SELECT StockPairs.symbol1 as symbol1, StockPairs.symbol2 as symbol2, "
+		+ "PairOnePrices.date as date, "
+		+ "PairOnePrices.adjusted_close as adjusted_close1, "
+		+ "PairTwoPrices.adjusted_close as adjusted_close2 "
+		+ "FROM StockPairs, PairOnePrices, PairTwoPrices "
+		+ "WHERE(((StockPairs.symbol1 = PairOnePrices.symbol) AND "
+		+ "(StockPairs.symbol2 = PairTwoPrices.symbol)) AND "
+		+ "(PairOnePrices.date = PairTwoPrices.date)) "
+		+ "ORDER BY symbol1, symbol2) as adjClose "
+		+ "WHERE StockPairs.symbol1 = adjClose.symbol1 AND StockPairs.symbol2 = adjClose.symbol2 AND adjClose.date <= \'"
+		+ back_test_start_date + "\');";
+	if (ExecuteSQL(db, calculate_volatility_for_pair.c_str()) == -1)
+		return -1;
+	cout << "Successfully calculate volatility for pairs." << endl;
+	
+	return 0;
+}
 
 int main() {
 
@@ -95,6 +161,7 @@ int main() {
 	string database_name = "PairTrading.db";
 	string pairtxt = "PairTrading.txt";
 	vector<string> PairOneSymbols, PairTwoSymbols;
+	string back_test_start_date = "2021-12-31";
 	char user_option;
 	if (OpenDatabase(database_name.c_str(), db) != 0)	  return -1;
 	
@@ -104,6 +171,8 @@ int main() {
 		cout << "-----------menu-------------" << endl;
 		cout << "A - Create and Populate Pair Table" << endl;
 		cout << "B - Retrieve adn Populate Historical Data for Each Stock" << endl;
+		cout << "C - Create and Populate Pair Table" << endl;
+		cout << "D - Calculate Volatility" << endl;
 		cout << "X - Exit" << endl << endl;;
 
 
@@ -122,6 +191,13 @@ int main() {
 			cout << "successfully populate pair one data" << endl;
 			if (PullPairDataToDB(PairTwoSymbols, db, false) == -1) return -1;
 			cout << "successfully populate pair two data" << endl;
+			break;
+		case 'C':
+			if (CreatePairPricesTable(db) == -1) return -1;
+			if (PopulatePairPrices(db) == -1) return -1;
+			break;
+		case 'D':
+			if (CalculateVolatility(db, back_test_start_date) == -1) return -1;
 			break;
 		case 'X':
 			exit(0);
